a0f875ca17c52f9585df09fc4196a25adb242863
ppxb <317842449@qq.com>
2026-01-30 12:29:08 +0800

    lint: format code

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,14 +1,15 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_VERSION: string
-  readonly VITE_APP_PORT: string
-  readonly VITE_BASE_URL: string
-  readonly VITE_API_PREFIX: string
-  readonly VITE_API_PROXY_URL: string
-  readonly VITE_ENABLE_DEVTOOLS: string
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_VERSION: string;
+  readonly VITE_APP_PORT: string;
+  readonly VITE_BASE_URL: string;
+  readonly VITE_API_PREFIX: string;
+  readonly VITE_API_PROXY_URL: string;
+  readonly VITE_ENABLE_DEVTOOLS: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
