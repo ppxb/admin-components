e1f7832291ff02d2ed70175b2cdb78eb9a07af77
ppxb <317842449@qq.com>
2026-01-30 12:52:30 +0800

    feat: add html plugin for vite

--- plugins/index.ts
+++ plugins/index.ts
@@ -5,12 +5,14 @@ import vue from '@vitejs/plugin-vue';
 import createAppInfoPlugin from './app-info';
 import createCompressionPlugin from './compression';
 import createDevtools from './devtools';
+import createHtmlPlugin from './html';
 
 export default function createVitePlugins(viteEnv: ImportMetaEnv) {
-  const { VITE_ENABLE_DEVTOOLS, VITE_APP_VERSION } = viteEnv;
+  const { VITE_ENABLE_DEVTOOLS, VITE_APP_VERSION, VITE_APP_TITLE } = viteEnv;
 
   const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), tailwindcss()];
   vitePlugins.push(createAppInfoPlugin(VITE_APP_VERSION));
+  vitePlugins.push(createHtmlPlugin(VITE_APP_TITLE));
   vitePlugins.push(createCompressionPlugin());
   vitePlugins.push(createDevtools(VITE_ENABLE_DEVTOOLS));
   vitePlugins.push();
