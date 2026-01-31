bc9ffd84e2a17b4eb73c62f25b7f417869c263e3
ppxb <317842449@qq.com>
2026-01-31 10:35:11 +0800

    refactor: i18n support

--- src/views/auth/components/login-form.vue
+++ src/views/auth/components/login-form.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
