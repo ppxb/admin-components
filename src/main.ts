34cee65141ed7f3325561bfc86754e6c3ccb2655
ppxb <317842449@qq.com>
2026-01-31 10:33:26 +0800

    chore: add i18n

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue';
 
+import { setupI18n } from '@/locales';
 import App from './App.vue';
+
 import '@/styles/index.css';
 
-function bootstrap() {
+async function bootstrap() {
   const app = createApp(App);
+
+  await setupI18n(app);
 
   app.mount('#app');
 }
