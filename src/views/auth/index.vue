06dfc7d957eb426923468e1be96517a1d8126793
ppxb <317842449@qq.com>
2026-02-01 10:43:35 +0800

    refactor: language toggle component

--- src/views/auth/index.vue
+++ src/views/auth/index.vue
@@ -1,17 +1,6 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { Button } from '@/components/ui/button';
-import { i18n, loadLocaleMessages } from '@/locales';
+import ToggleLanguage from '@/components/toggle-language.vue';
 import LoginForm from './components/login-form.vue';
-
-const currentLocale = computed(() => i18n.global.locale.value);
-
-const isZhCN = computed(() => currentLocale.value === 'zh-CN');
-
-async function toggleLanguage() {
-  const targetLang = isZhCN.value ? 'en-US' : 'zh-CN';
-  await loadLocaleMessages(targetLang);
-}
 </script>
 
 <template>
@@ -19,16 +8,8 @@ async function toggleLanguage() {
     className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10"
   >
     <div className="w-full max-w-sm md:max-w-4xl">
-      <div class="absolute top-4 right-4 rounded-xl border md:top-6 md:right-6">
-        <Button
-          variant="ghost"
-          size="sm"
-          type="button"
-          class="rounded-xl"
-          @click="toggleLanguage"
-        >
-          {{ isZhCN ? 'English' : '简体中文' }}
-        </Button>
+      <div class="absolute top-6 right-6 md:top-6 md:right-6">
+        <ToggleLanguage />
       </div>
       <LoginForm />
     </div>
