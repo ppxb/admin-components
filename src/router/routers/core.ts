3e4f08877563fcf22174b8c0bf91bdc12bbc8115
ppxb <317842449@qq.com>
2026-02-04 16:20:36 +0800

    chore: change index path to login

--- src/router/routers/core.ts
+++ src/router/routers/core.ts
@@ -1,5 +1,16 @@
 import type { RouteRecordRaw } from 'vue-router';
 
+const coreRoutes: RouteRecordRaw[] = [
+  {
+    component: () => import('@/views/auth/index.vue'),
+    meta: {
+      title: 'Auth',
+    },
+    name: 'Auth',
+    path: '/',
+  },
+];
+
 const fallbackNotFoundRoute: RouteRecordRaw = {
   component: () => import('@/views/_core/fallback/not-found.vue'),
   meta: {
@@ -8,14 +19,5 @@ const fallbackNotFoundRoute: RouteRecordRaw = {
   name: 'FallbackNotFound',
   path: '/:path(.*)*',
 };
-const coreRoutes: RouteRecordRaw[] = [
-  {
-    component: () => import('@/views/auth/index.vue'),
-    meta: {
-      title: 'Auth',
-    },
-    name: 'Auth',
-    path: '/auth',
-  },
-];
+
 export { coreRoutes, fallbackNotFoundRoute };
