d60d14392d047d78e73fad8a91c352cabc3e38b9
ppxb <317842449@qq.com>
2026-02-07 13:27:25 +0800

    chore: add eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import antfu from '@antfu/eslint-config';
+import prettier from 'eslint-config-prettier';
 
 export default antfu({
   stylistic: false,
-  formatters: false,
-});
+}).append(prettier);
